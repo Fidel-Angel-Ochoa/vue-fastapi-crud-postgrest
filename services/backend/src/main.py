a17d2aac0d079dffa995781c91d618f16cba3a66
fidel-angel-ochoa <fidel.angel.ochoa@gmail.com>
2023-08-19 21:18:14 -0700

    adding frontend url env variable to main.py

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
+    allow_origins=["URL_FRONTEND"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
